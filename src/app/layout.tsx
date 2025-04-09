0411a83289f962d779116c732db7f9915b6d3f96
sheheer <muhammedsheheer99@gmail.com>
2025-04-09 16:27:23 +0530

    layout name added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,11 @@ import "@/styles/lines.css";
 import "@/styles/ham.css";
 import { cn } from "@/lib/utils";
 import { type Metadata } from "next";
-import {
-  Open_Sans,
-  Sofia_Sans_Condensed,
-  Poppins,
-} from "next/font/google";
+import { Open_Sans, Sofia_Sans_Condensed, Poppins } from "next/font/google";
 import Providers from "@/app/Providers";
 
 export const metadata: Metadata = {
-  title: "Blends Cafe",
+  title: "template1",
   description: "Blends Cafe",
   icons: [{ rel: "icon", url: "/images/home/hero/logo.png", sizes: "11x11" }],
 };
@@ -22,20 +18,17 @@ const sofia_sans = Sofia_Sans_Condensed({
   weight: ["400", "500", "600", "700", "800"],
 });
 
-
 const open_sans = Open_Sans({
   subsets: ["latin"],
   variable: "--font-open_sans",
   weight: ["400", "500", "600", "700", "800"],
 });
 
-
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
   weight: ["400", "500", "600", "700", "800"],
 });
-
 
 export default function RootLayout({
   children,
@@ -44,7 +37,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "theme-custom flex min-h-screen font-inter antialiased",
+          "theme-custom font-inter flex min-h-screen antialiased",
           sofia_sans.variable,
           open_sans.variable,
           poppins.variable,
